6673e64ba08820362650c853667ed19206c1e1a7
Abu Ameen <abuameen.student@gmail.com>
2025-01-13 00:00:23 +0530

    update test

--- band_name_generator/test_main.py
+++ band_name_generator/test_main.py
@@ -1,8 +1,6 @@
-import pytest
-
 from main import generate_band_name
 
 
 def test_band_name():
-    assert generate_band_name("city_name", "pet_name") == "city_name pet_name"
+    assert generate_band_name("city_name", "pet_name") == "Your band name could be city_name pet_name"
     
