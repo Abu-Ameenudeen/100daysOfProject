af03ca2326302c32ea2bb85fd7d0d75e8a4a404b
Abu Ameen <abuameen.student@gmail.com>
2025-01-12 23:58:45 +0530

    Update band function

--- band_name_generator/main.py
+++ band_name_generator/main.py
@@ -3,11 +3,12 @@ def main():
     city = input("What's the name of the city you grew up in?\n")
     pet_name = input("What's your pet's name?\n")
     output = generate_band_name(city, pet_name)
+    print("")
     print(output)
     
 
 def generate_band_name(city_name, pet_name):
-    return f"{city_name} {pet_name}"
+    return f"Your band name could be {city_name} {pet_name}"
 
     
 if __name__ == "__main__":
